28ac769342940de30aa55204f5db8e6fdb7a9cff
Toni Ruottu <toni.ruottu@iki.fi>
2025-06-04 00:26:05 +0300

    lib2

--- src/lib.py
+++ src/lib.py
@@ -1,69 +1,99 @@
 #!/usr/bin/env python3
 
-"""
->>> example = 'ab-12 cd-34 ab-56 cd-78'
->>> list(chop(' ', example))
-['ab-12', ' ', 'cd-34', ' ', 'ab-56', ' ', 'cd-78']
->>> list(chop('- ', example))
-['ab', '-', '12', ' ', 'cd', '-', '34', ' ', 'ab', '-', '56', ' ', 'cd', '-', '78']
->>> list(chop('ba78-', example))
-['ab-', '12 cd', '-', '34 ', 'ab-', '56 cd', '-78']
->>> list(split(' ', example))
-['ab-12', 'cd-34', 'ab-56', 'cd-78']
->>> list(split('- ', example))
-['ab', '12', 'cd', '34', 'ab', '56', 'cd', '78']
->>> list(split('ba78-', example))
-['', '12 cd', '34 ', '56 cd', '']
->>> list(match(' ', example))
-[' ', ' ', ' ']
->>> list(match('- ', example))
-['-', ' ', '-', ' ', '-', ' ', '-']
->>> list(match('ba78-', example))
-['ab-', '-', 'ab-', '-78']
-"""
+class Pipe:
+    """
+    >>> Pipe(3) | (lambda x: 2 * x) | None
+    6
+    """
+    def __init__(self, raw):
+        self._raw = raw
 
-import string
+    def __or__(self, f):
+        if f is None:
+            return self._raw
+        return Pipe(f(self._raw))
 
-def chop(chars, line):
-    sep = None
-    buf = ''
-    for i in range(len(line)):
-        c = line[i]
-        if c in chars:
-            if sep == None:
-               sep = True
-            if sep:
-                buf += c
+def flatten(xss):
+    """
+    >>> flatten([['ab', 12], ['cd', 34]])
+    ['ab', 12, 'cd', 34]
+    """
+    return [x for xs in xss for x in xs]
+
+def split(chars):
+    """
+    >>> example = 'ab-12 cd-34 ab-56 cd-78'
+    >>> Pipe(example) | split(' ') | list | None
+    ['ab-12', ' ', 'cd-34', ' ', 'ab-56', ' ', 'cd-78']
+    >>> Pipe(example) | split('- ') | list | None
+    ['ab', '-', '12', ' ', 'cd', '-', '34', ' ', 'ab', '-', '56', ' ', 'cd', '-', '78']
+    >>> Pipe(example) | split('ba78-') | list | None
+    ['', 'ab-', '12 cd', '-', '34 ', 'ab-', '56 cd', '-78', '']
+    """
+    def _split(string):
+        sep = False
+        buf = ''
+        for c in string:
+            if c in chars:
+                if sep:
+                    buf += c
+                else:
+                    yield buf
+                    sep = True
+                    buf = c
             else:
-                yield buf
-                sep = True
-                buf = c
-        else:
-            if sep == None:
-               sep = False
-            if sep:
-                yield buf
-                sep = False
-                buf = c
-            else:
-                buf += c
-    yield buf
+                if sep:
+                    yield buf
+                    sep = False
+                    buf = c
+                else:
+                    buf += c
+        yield buf
+        if sep:
+          yield ''
+    return _split
 
-def split(chars, line):
-    out = True
-    for part in chop(chars, line):
-        if out:
-            yield part
-        out = not(out)
+def pick(indices):
+    """
+    >>> Pipe('abcdefg') | pick([1, 2, 3]) | list | None
+    ['a', 'c', 'e']
+    >>> Pipe('ab') | pick([1, 2, 3]) | list | None
+    ['a', '', '']
+    """
+    def _pick(chunks):
+        for i in indices:
+            try:
+                yield chunks[(i - 1) * 2]
+            except IndexError:
+                yield ''
+    return _pick
 
-def match(chars, line):
-    out = False
-    for part in chop(chars, line):
-        if out:
-            yield part
-        out = not(out)
+def concat(indices):
+    """
+    >>> Pipe('abcdefg') | concat([1, 2, 3]) | list | None
+    ['ace']
+    >>> Pipe('ab') | concat([1, 2, 3]) | list | None
+    ['a']
+    """
+    def _concat(chunks):
+        return [''.join(pick(indices)(chunks))]
+    return _concat
 
-    
+def blend(chop, *glues):
+    """
+    >>> example = 'ab-12 cd-34 ab-56 cd-78'
+    >>> Pipe([example]) | blend(split('- '), concat([2, 4]), concat([4, 2])) | list | None
+    ['1234', '3412']
+    >>> Pipe([example]) | blend(split(' '), pick([1, 3])) | blend(split('-'), pick([2])) | list | None
+    ['12', '56']
+    """
+    def _blend(strings):
+        for string in strings:
+            chunks = list(chop(string))
+            for glue in glues:
+                yield from glue(chunks)
+    return _blend
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
