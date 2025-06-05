55d61b115ac95a9ad4fa023ace113059b4a8edbc
Toni Ruottu <toni.ruottu@iki.fi>
2025-06-05 20:37:22 +0300

    structs

--- src/lib.py
+++ src/lib.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from typing import TypedDict
+
 class Pipe:
     """
     >>> Pipe(3) | (lambda x: 2 * x) | None
@@ -20,62 +22,77 @@ def flatten(xss):
     """
     return [x for xs in xss for x in xs]
 
-def split(chars):
+
+class Split(TypedDict):
+    pattern: str
+    fields: list[str]
+    junk: list[str]
+
+def split(pattern: str):
     """
     >>> example = 'ab-12 cd-34 ab-56 cd-78'
-    >>> Pipe(example) | split(' ') | list | None
-    ['ab-12', ' ', 'cd-34', ' ', 'ab-56', ' ', 'cd-78']
-    >>> Pipe(example) | split('- ') | list | None
-    ['ab', '-', '12', ' ', 'cd', '-', '34', ' ', 'ab', '-', '56', ' ', 'cd', '-', '78']
-    >>> Pipe(example) | split('ba78-') | list | None
-    ['', 'ab-', '12 cd', '-', '34 ', 'ab-', '56 cd', '-78', '']
+    >>> Pipe(example) | split(' ') | None
+    {'pattern': ' ', 'fields': ['ab-12', 'cd-34', 'ab-56', 'cd-78'], 'junk': [' ', ' ', ' ']}
+    >>> Pipe(example) | split('- ') | None
+    {'pattern': '- ', 'fields': ['ab', '12', 'cd', '34', 'ab', '56', 'cd', '78'], 'junk': ['-', ' ', '-', ' ', '-', ' ', '-']}
+    >>> Pipe(example) | split('ba78-') | None
+    {'pattern': 'ba78-', 'fields': ['', '12 cd', '34 ', '56 cd', ''], 'junk': ['ab-', '-', 'ab-', '-78']}
     """
-    def _split(string):
+    def _split(string: str) -> Split:
+        result: Split = {
+            'pattern': pattern,
+            'fields': [],
+            'junk': [],
+	}
         sep = False
         buf = ''
         for c in string:
-            if c in chars:
+            if c in pattern:
                 if sep:
                     buf += c
                 else:
-                    yield buf
+                    result['fields'].append(buf)
                     sep = True
                     buf = c
             else:
                 if sep:
-                    yield buf
+                    result['junk'].append(buf)
                     sep = False
                     buf = c
                 else:
                     buf += c
-        yield buf
         if sep:
-          yield ''
+            result['junk'].append(buf)
+            result['fields'].append('')
+        else:
+            result['fields'].append(buf)
+        return result
+
     return _split
 
 def pick(indices):
     """
-    >>> Pipe('abcdefg') | pick([1, 2, 3]) | list | None
-    ['a', 'c', 'e']
-    >>> Pipe('ab') | pick([1, 2, 3]) | list | None
+    >>> Pipe({ 'fields': ['a', 'c', 'e', 'g'], 'junk': ['b', 'd', 'f'] }) | pick([4, 1, 3, 1]) | list | None
+    ['g', 'a', 'e', 'a']
+    >>> Pipe({ 'fields': ['a'], 'junk': ['b'] }) | pick([1, 2, 3]) | list | None
     ['a', '', '']
     """
-    def _pick(chunks):
+    def _pick(chunks: Split):
         for i in indices:
             try:
-                yield chunks[(i - 1) * 2]
+                yield chunks['fields'][i - 1]
             except IndexError:
                 yield ''
     return _pick
 
 def concat(indices):
     """
-    >>> Pipe('abcdefg') | concat([1, 2, 3]) | list | None
-    ['ace']
-    >>> Pipe('ab') | concat([1, 2, 3]) | list | None
+    >>> Pipe({ 'fields': ['a', 'c', 'e', 'g'], 'junk': ['b', 'd', 'f'] }) | concat([4, 1, 3, 1]) | list | None
+    ['gaea']
+    >>> Pipe({ 'fields': ['a'], 'junk': ['b'] }) | concat([1, 2, 3]) | list | None
     ['a']
     """
-    def _concat(chunks):
+    def _concat(chunks: Split):
         return [''.join(pick(indices)(chunks))]
     return _concat
 
@@ -89,7 +106,7 @@ def blend(chop, *glues):
     """
     def _blend(strings):
         for string in strings:
-            chunks = list(chop(string))
+            chunks = chop(string)
             for glue in glues:
                 yield from glue(chunks)
     return _blend
